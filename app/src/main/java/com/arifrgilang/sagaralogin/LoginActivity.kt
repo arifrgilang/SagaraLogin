12e4ac5e86d61870f2eb1fc3525311874ad3d516
Arif R Gilang <arifrgilang@gmail.com>
2019-11-20 18:17:05 +0700

    Initial commit

---
+++ app/src/main/java/com/arifrgilang/sagaralogin/LoginActivity.kt
@@ -0,0 +1,36 @@
+package com.arifrgilang.sagaralogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        choose_login_button.setOnClickListener {
+            if(username.text.isNullOrEmpty() || password.text.isNullOrEmpty()){
+                Toast.makeText(this,
+                    "Isi form login!",
+                    Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                if(username.text!!.toString().equals("arif@sagara.asia") && password.text!!.toString().equals("sagaraasia")){
+                    startActivity(Intent(this,MainActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(this,
+                        "E-mail atau Password salah!",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
+        registration_button.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/arifrgilang/sagaralogin/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.arifrgilang.sagaralogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val fadeAnim = AnimationUtils.loadAnimation(this, R.anim.fadein)
+        splash_image.startAnimation(fadeAnim)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
