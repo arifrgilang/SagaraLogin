929f2499908d7992d4fdb4da6d722c8af1b8e236
Arif R Gilang <arifrgilang@gmail.com>
2019-12-03 20:32:10 +0700

    Menyambung DB dan integrasi login ke database

--- app/src/main/java/com/arifrgilang/sagaralogin/LoginActivity.kt
+++ app/src/main/java/com/arifrgilang/sagaralogin/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.arifrgilang.sagaralogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private val db = FirebaseDatabase.getInstance()
+    private lateinit var dbRef : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,16 +23,37 @@ class LoginActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT)
                     .show()
             } else {
-                if(username.text!!.toString().equals("arif@sagara.asia") && password.text!!.toString().equals("sagaraasia")){
-                    startActivity(Intent(this,MainActivity::class.java))
-                    finish()
-                } else {
-                    Toast.makeText(this,
-                        "E-mail atau Password salah!",
-                        Toast.LENGTH_SHORT)
-                        .show()
+                val id = username.text!!.toString()
+                val pw = password.text!!.toString()
+
+                when(id){
+                    "arifrgilang" -> checkLogin(id, pw)
+                    "ewok" -> checkLogin(id, pw)
+                    else -> {
+                        Toast.makeText(this,
+                            "Username not found!",
+                            Toast.LENGTH_SHORT)
+                            .show()
+                    }
                 }
             }
         }
+    }
+
+    private fun checkLogin(id: String, pw: String){
+        // Masukkan logic untuk Enkripsi PW
+        dbRef = db.reference.child("account").child(id)
+        dbRef.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                val value = p0.getValue(String::class.java)
+                Log.d("onDataChange", value!!)
+            }
+            override fun onCancelled(p0: DatabaseError) {
+                Log.d("OnCancelled", p0.message)
+                // Masukkan logic untuk Dekripsi password
+                // If password yang di dekripsi sama dengan PW
+                // Maka StartActivity
+            }
+        })
     }
 }
